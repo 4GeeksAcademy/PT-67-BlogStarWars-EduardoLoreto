4e8705a85e407293289ead8dfb95ffb0acb54984
EduardoLoreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-06-17 17:27:20 +0000

    Creacion de Character, Planets & vehicles

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect }from "react";
+
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		
-	</div>
-);
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect(()=> {
+		 actions.getCharacters()
+	}, [])
+
+
+	return(
+		<div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+			<div className="carousel-inner d-flex justify-content-center w-100">
+				{store.characters.map((item, index ) => {
+					return (
+							<div className={`card carousel-item ${index ===0 && "active"}`} data-bs-interval="2000" style={{width: "18rem"}}>
+								<img src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} className="card-img-top" alt="..."/>
+								<div className="card-body">
+									<h5 className="card-title">{item.name}</h5>
+									<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									<Link to={`/characthers/${item.uid}`} className="btn btn-primary">Go somewhere</Link>
+								</div>
+							</div>
+					)
+				})}
+			</div>
+			<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+				<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span className="visually-hidden">Previous</span>
+			</button>
+			<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+				<span className="carousel-control-next-icon" aria-hidden="true"></span>
+				<span className="visually-hidden">Next</span>
+			</button>
+		</div>
+
+)};
---
+++ src/js/views/src/js/views/charactersDetails.js
@@ -0,0 +1,15 @@
+import React, { useContext }from "react";
+
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+
+export const CharactersDetails = () => {
+
+
+	return(
+
+	<div className="text-center mt-5">
+		<h1>characters!</h1>
+	</div>
+)};
